e193a324d31d1920afefd1915c6d95c0650e1410
sridharveigandla <sridhar.veigandla05@gmail.com>
2021-06-02 14:31:58 +0530

    intial commit

--- src/HeaderNew.js
+++ src/HeaderNew.js
@@ -251,7 +251,7 @@ export default function HeaderNew() {
         <Typography paragraph>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
           ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+          facilisis lintaeo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
           gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
           donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
           adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
